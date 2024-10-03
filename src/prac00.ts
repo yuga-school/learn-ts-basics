2dbb67fc9ed8425adb420f323dbcf51486557128
higashi <higashiyuga1@gmail.com>
2024-10-03 10:06:59 +0900

    first commit

---
+++ src/prac00.ts
@@ -0,0 +1,13 @@
+export {}; // ファイルをモジュールとして扱いスコープを分離するために必要
+
+function greetAndCalculate(name: string, a: number, b: number): string {
+  const sum: number = a + b;
+  return `Hello, ${name}! The sum of ${a} and ${b} is ${sum}`;
+}
+
+const userName: string = "Alice";
+const x: number = 10;
+const y: number = 20;
+
+const result: string = greetAndCalculate(userName, x, y);
+console.log(result);
---
+++ src/prac01.ts
@@ -0,0 +1,3 @@
+export {}; // ファイルをモジュールとして扱いスコープを分離するために必要
+const userName: string = "Bob";
+console.log(`Hi, ${userName}.`);
